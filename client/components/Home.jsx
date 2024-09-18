63c34049f2333f1ea0ab6231501cc5bf7470b3a2
rushfpcent <sfazelpo@my.centennialcollege.ca>
2024-09-18 12:20:28 -0400

    commit

--- client/components/Home.jsx
+++ client/components/Home.jsx
@@ -1,5 +1,9 @@
 export default function Home() {
     return (
-    <p>Home text</p>
+        <div>
+        <p>Hello! Welcome to my personal page!</p>
+        <p>My name is Soroush and I am a student in Centennial College's Software Engineering Technology program with a focus on Artificial Intelligence and Machine Learning. </p>
+        <p>On this site you can find more information about me and my skill set.</p>
+        </div>
     );
 }
