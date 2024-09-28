ca3c14c1396e9d412edf94aa962fb8200253df26
rushfpcent <sfazelpo@my.centennialcollege.ca>
2024-09-28 17:52:33 -0400

    commit

--- client/components/About.jsx
+++ client/components/About.jsx
@@ -1,15 +1,8 @@
-import PDFViewer from "./PDF";
-
 export default function About() {
     return (
         <>
             <p>About Me</p>
-            <div className="App" style={{ width: '100vw' }}>
-			    <div className="PDF-viewer">
-				    <PDFViewer document={'document.pdf'} />
-			    </div>
-		    </div>
-
+            
         </>
         
     );
