5e5ce62e6648fe562076f68fbcdf2ef3afdf2ea7
rushfpcent <sfazelpo@my.centennialcollege.ca>
2024-09-28 22:38:58 -0400

    commit

--- client/src/components/About.jsx
+++ client/src/components/About.jsx
@@ -14,7 +14,7 @@ export default function About() {
         <>
             <p>About Me</p>
             <div className="pdf">
-            <PdfViewer pdfUrl="..\dist\document.pdf" />
+            <PdfViewer pdfUrl="./document.pdf" />
             </div>
         </>
         
