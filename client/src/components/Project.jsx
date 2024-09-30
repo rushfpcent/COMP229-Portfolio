2ad3a269778ffeb7ef7b44607b834da2aef73b98
rushfpcent <sfazelpo@my.centennialcollege.ca>
2024-09-29 20:31:57 -0400

    commit

--- client/src/components/Project.jsx
+++ client/src/components/Project.jsx
@@ -4,12 +4,32 @@ Student Name: Soroush Fazel-Pour
 Student ID: 301368534
 Date: September 29, 2024
 */
+import React from "react";
+import '../css/project.css';
 
 export default function Project() {
-    return(
+    return (
         <div className="project-container">
             <h2 className="project-header">My Projects</h2>
-
+            <div className="projects-columns">
+                <div className="project-column">                    
+                    <h3>Ontario's Premiers</h3>
+                    <p>Created a WinForm app that displays a scrollable list of the Premiers of Ontario</p>
+                    <p>Handled primary responsibility of creating the WinForm app and the dictionary containing the data</p>
+                    <img src="/project1.png" alt="Project 1" className="imgProject" />   
+                </div>
+                <div className="project-column">
+                    <h3>Course Evaluation Website Mockup</h3>
+                    <p>Created a website meant for submitting course evaluations as a form.</p>
+                    <img src="/project2.png" alt="Project 2" className="imgProject" />
+                </div>
+                <div className="project-column">
+                    <h3>Realtor Website Mockup</h3>
+                    <p>Created a multi-page website of a real-estate agent with 3 listings</p>
+                    <p>Each listing had numerous images with descriptions, displayed using a carousel.</p>
+                    <img src="project3.png" alt="Project 3" className="imgProject" />
+                </div>
+            </div>
         </div>
     );
 }
